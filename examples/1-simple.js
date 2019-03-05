8519ae16bf9f4b21030052cc8654ad9db63f096f
dxcweb <dxcweb@qq.com>
2019-03-05 14:10:11 +0800

    no message

--- examples/1-simple.js
+++ examples/1-simple.js
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./ApiAuth";
 import { netFetch } from "dxc-material";
-import { openMaterial, authSign } from "dxc-material";
-
+import { openMaterial, catchremoteimage } from "dxc-material";
+import loadjs from 'dxc-loadjs';
 class Demo extends React.Component {
+
+  componentDidMount() {
+    this.loadUeditor();
+  }
+  loadUeditor = async () => {
+    if (!window.UE) {
+      const homeUrl = (window.UEDITOR_HOME_URL = 'https://code.tuobacco.com/ueditor/1-4-3-2/');
+      window.UEDITOR_DIALOGS_PATH = '';
+      await loadjs(`${homeUrl}ueditor.min.js`);
+    }
+    this.ue = window.UE.getEditor('editor');
+    window.ue = this.ue;
+    catchremoteimage(this.ue);
+  };
   onNetFetch = async () => {
     const apiRes = await netFetch([
       "https://xiumi.us/images/app/home/0cc68a.icon-paper-guide.png",
@@ -27,6 +41,8 @@ class Demo extends React.Component {
       <div style={{ padding: 30, lineHeight: 2 }}>
         <div onClick={openMaterial}>素材库</div>
         <div onClick={this.onNetFetch}>网络资源下载</div>
+
+        <script style={{ width: 400, maxWidth: "100%", height: 500 }} id="editor" type="text/plain" />
       </div>
     );
   }
