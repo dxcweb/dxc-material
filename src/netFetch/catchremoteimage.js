aa7f079dc2756248d0da4958dec3c8d13a0c609b
dxcweb <dxcweb@qq.com>
2019-04-01 21:05:03 +0800

    no message

--- src/netFetch/catchremoteimage.js
+++ src/netFetch/catchremoteimage.js
@@ -1,80 +1,73 @@
 import netFetch from "./netFetch";
-export default (editor) => {
-  const me = editor;
-  const { domUtils } = window.UE.dom;
-  me.addListener("afterpaste", function() {
-    me.fireEvent("catchRemoteImage");
-  });
-  me.addListener("catchRemoteImage", function() {
-    var catcherLocalDomain = me.getOpt("catcherLocalDomain");
-    var remoteImages = [],
-      imgs = domUtils.getElementsByTagName(me.document, "img"),
-      test = function(src, urls) {
-        if (src.indexOf(window.location.host) != -1 || /(^\.)|(^\/)/.test(src)) {
-          return true;
-        }
-        if (urls) {
-          for (var j = 0, url; (url = urls[j++]); ) {
-            if (src.indexOf(url) !== -1) {
-              return true;
-            }
-          }
-        }
-        return false;
-      };
+// import { netFetch } from 'dxc-material';
+import loading from 'fs-loading';
 
-    for (var i = 0, ci; (ci = imgs[i++]); ) {
-      if (ci.getAttribute("word_img")) {
-        continue;
-      }
-      var src = ci.getAttribute("_src") || ci.src || "";
-      if (/^(https?|ftp):/i.test(src) && !test(src, catcherLocalDomain)) {
-        remoteImages.push(src);
+const getLocalUrl = async remote => {
+  const apiRes = await netFetch([remote]);
+  if (apiRes.status !== 200) {
+    alert('请求错误');
+    return;
+  }
+  const { data, result, msg } = apiRes.data;
+  if (!result) {
+    alert(msg);
+    return;
+  }
+  return data[0].url;
+};
+
+const backgroundImage = async style => {
+  const bgIndex = style.indexOf('background-image') || style.indexOf('background');
+  if (bgIndex >= 0) {
+    const endInde = style.indexOf(';', bgIndex);
+    if (endInde >= 0) {
+      let start = style.indexOf('url(', bgIndex);
+      if (start >= 0 && start < endInde) {
+        start += 4;
+        const end = style.indexOf(')', start);
+        const original = style.substr(start, end - start);
+        const really = original
+          .replace(/&quot;/g, '')
+          .replace(/;/g, '')
+          .replace(/"/g, '')
+          .replace(/'/g, '');
+
+        const url = await getLocalUrl(really);
+        return style.replace(original, url);
       }
     }
-
-    if (remoteImages.length) {
-      catchremoteimage(remoteImages, {
-        //成功抓取
-        success: function(list) {
-          /* 获取源路径和新路径 */
-          var i, j, ci, cj, oldSrc, newSrc;
-
-          for (i = 0; (ci = imgs[i++]); ) {
-            oldSrc = ci.getAttribute("_src") || ci.src || "";
-            for (j = 0; (cj = list[j++]); ) {
-              if (oldSrc == cj.source) {
-                //抓取失败时不做替换处理
-                newSrc = cj.url;
-                domUtils.setAttributes(ci, {
-                  src: newSrc,
-                  _src: newSrc,
-                });
-                break;
-              }
-            }
-          }
-          me.fireEvent("catchremotesuccess");
-        },
-        //回调失败，本次请求超时
-        error: function() {
-          me.fireEvent("catchremoteerror");
-        },
-      });
-    }
-
-    async function catchremoteimage(imgs, callbacks) {
-      const apiRes = await netFetch(imgs);
-      if (apiRes.status !== 200) {
-        alert("请求错误");
-        return;
+  }
+  return false;
+};
+const catchImage = async node => {
+  if (node.type === 'element') {
+    if (node.attrs && node.attrs.style) {
+      const newStyle = await backgroundImage(node.attrs.style);
+      if (newStyle) {
+        node.attrs.style = newStyle;
       }
-      const { data, result, msg } = apiRes.data;
-      if (!result) {
-        alert(msg);
-        return;
+      if (node.tagName === 'img' && node.attrs && node.attrs.src) {
+        const url = await getLocalUrl(node.attrs.src);
+        node.attrs.src = url;
+        if (node.attrs._src) node.attrs._src = url;
       }
-      callbacks.success(data);
     }
+  }
+  if (node.children) {
+    for (let i = 0; i < node.children.length; i++) {
+      await catchImage(node.children[i]);
+    }
+  }
+};
+const handle = async (editor, node) => {
+  loading.show();
+  await catchImage(node);
+  editor.execCommand('insertHtml', node.toHtml(), true);
+  loading.hide();
+};
+export default editor => {
+  editor.addListener('beforepaste', function(Event, html, node) {
+    html.html = false;
+    handle(editor, node);
   });
 };
